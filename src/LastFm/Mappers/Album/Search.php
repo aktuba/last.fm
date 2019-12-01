c6ef9795e97393e370e429c893e4bfb947866d0e
Ruslan Kalinov <aktuba@yandex.ru>
2019-12-01 20:41:13 +0300

    bug fix

--- src/LastFm/Mappers/Album/Search.php
+++ src/LastFm/Mappers/Album/Search.php
@@ -12,7 +12,7 @@ use Tightenco\Collect\Support\Collection;
  * @package aktuba\LastFm\Mappers\Album
  *
  * @property Mappers\Query $query
- * @property Collection $result
+ * @property Collection $results
  * @property Mappers\MetaFor $meta
  */
 class Search extends JsonMapper
